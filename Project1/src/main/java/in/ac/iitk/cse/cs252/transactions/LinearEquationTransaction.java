4141ee25042c73df73464c962e9310fa5d68797e
Asim Unmesh <a.unmesh@gmail.com>
2017-10-12 09:29:49 +0530

    added redemption script

--- Project1/src/main/java/in/ac/iitk/cse/cs252/transactions/LinearEquationTransaction.java
+++ Project1/src/main/java/in/ac/iitk/cse/cs252/transactions/LinearEquationTransaction.java
@@ -22,6 +22,9 @@ public class LinearEquationTransaction extends ScriptTransaction {
     public LinearEquationTransaction(NetworkParameters parameters, File file, String password) {
         super(parameters, file, password);
     }
+    
+    private int x = 5620;   
+    private int y = 4580; //Values for 
 
     @Override
     public Script createInputScript() {
@@ -58,8 +61,29 @@ public class LinearEquationTransaction extends ScriptTransaction {
     @Override
     public Script createRedemptionScript(Transaction unsignedScript) {
         // TODO: Create a spending script
-    	
-        return null;
+
+        ScriptBuilder builder = new ScriptBuilder();
+      
+        ByteBuffer X_var = ByteBuffer.allocate(4);
+        X_var.order(ByteOrder.LITTLE_ENDIAN); // optional, the initial order of a byte buffer is always BIG_ENDIAN.
+        X_var.putInt(x);
+        
+        byte[] X = X_var.array();
+      
+
+        builder.data(X);
+      
+        ByteBuffer Y_var = ByteBuffer.allocate(4);
+        Y_var.order(ByteOrder.LITTLE_ENDIAN); // optional, the initial order of a byte buffer is always BIG_ENDIAN.
+        Y_var.putInt(y);
+        
+        byte[] Y = Y_var.array();
+      
+
+        builder.data(Y);
+
+
+        return builder.build();
     }
 
     private byte[] encode(BigInteger bigInteger) {
